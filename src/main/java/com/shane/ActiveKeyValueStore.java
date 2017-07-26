8ab62fd35c079c7cea45cc60a3e4907e6a780729
suchengen <suchengen@gmail.com>
2017-07-26 18:08:33 +0800

    first

--- src/main/java/com/shane/ActiveKeyValueStore.java
+++ src/main/java/com/shane/ActiveKeyValueStore.java
@@ -1,7 +1,27 @@
 package com.shane;
 
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooDefs;
+
+import java.nio.charset.Charset;
+
 /**
  * Created by Shane-PC on 2017/7/26.
  */
-public class ActiveKeyValueStore {
+public class ActiveKeyValueStore extends ConnectionWatcher {
+
+    private static final Charset CHARSET = Charset.forName("UTF-8");
+    public void write(String path, String value) throws KeeperException, InterruptedException {
+        if (null == (zk.exists(path, false))) {
+            zk.create(path, value.getBytes(CHARSET), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+        } else {
+            zk.setData(path, value.getBytes(CHARSET), -1);
+        }
+    }
+    public String read(String path, Watcher watcher) throws KeeperException, InterruptedException {
+        byte[] data = zk.getData(path, watcher, null);
+        return new String(data, CHARSET);
+    }
 }
