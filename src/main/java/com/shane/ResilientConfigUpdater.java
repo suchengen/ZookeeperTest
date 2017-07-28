7fa362eeac82f7f63c13751ba26588a9bfdddb57
suchengen <suchengen@gmail.com>
2017-07-28 11:05:43 +0800

    sadgdsfsad

--- src/main/java/com/shane/ResilientConfigUpdater.java
+++ src/main/java/com/shane/ResilientConfigUpdater.java
@@ -18,6 +18,8 @@ public class ResilientConfigUpdater extends ConnectionWatcher {
     public ResilientConfigUpdater(String hosts) throws IOException, InterruptedException {
         store = new ChangedActiveKeyValueStore();
         store.connect(hosts);
+
+
     }
 
     public void run() throws InterruptedException, KeeperException {
